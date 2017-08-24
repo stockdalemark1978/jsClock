d29832436c037f73b1ad74e29918a289f11e5373
stockdalemark1978 <stockdalemark1978@gmail.com>
2017-08-24 19:23:13 -0400

    added JS for hour hand

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 var secondHand = document.querySelector('.sec-hand');
+var hourHand = document.querySelector('.hour-hand');
+var minuteHand = document.querySelector('min-hand');
 
 //setting the date and getting the current time
 function setDate() {
@@ -7,9 +9,16 @@ function setDate() {
     var now = new Date();
     //getting the seconds of the current date and time
     var seconds = now.getSeconds();
-    var secondsDegrees = ((seconds / 60) * 360);
+    var secondsDegrees = ((seconds / 60) * 360) + 90;
     secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+    
+    //getting the hours of the current date and time
+    var hours = now.getHours();
+    var hoursDegrees = ((hours / 12) * 360) + 90;
+    hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
     console.log(seconds);
+
+
 }
 
 //setInterval causes the called function to be excuted every 1000 milliseconds
