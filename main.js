c34f28182396e7d76b5e397a849e3a26c6ed7546
stockdalemark1978 <stockdalemark1978@gmail.com>
2017-08-24 11:21:39 -0400

    adding more JS to get clock hands to work

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
+var secondHand = document.querySelector('.second-hand');
+
+//setting the date and getting the current time
 function setDate() {
-    console.log("I'm working!");
+    console.log(Date());
+    //getting current date and time
     var now = new Date();
+    //getting the seconds of the current date and time
     var seconds = now.getSeconds();
     console.log(seconds);
 }
 
+//setInterval causes the called function to be excuted every 1000 milliseconds
 setInterval(setDate, 1000);
